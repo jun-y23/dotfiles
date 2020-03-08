6ca5cd3930bccd7b952cf828d03a4e299c00f993
junun <ki8109smith@gmail.com>
2020-03-08 14:39:20 +0900

    second

--- dotfiles-installer.sh
+++ dotfiles-installer.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.vim/ ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gvimrc ~/.gvimrc
@@ -8,4 +6,4 @@ ln -s ~/dotfiles/.zsh/ ~/.zsh
 ln -s ~/dotfiles/.zshrc ~/.zshr
 
 chmod +x dotfiles-installer.sh
-./dotfiles-installer.sh
+bash ./dotfiles-installer.sh
