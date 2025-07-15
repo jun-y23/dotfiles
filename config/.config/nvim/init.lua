3f0316ce31aea359efecf5a19e31a88195368ffd
jun-y23 <ki8109smith@gmail.com>
2025-07-16 08:56:59 +0900

    add keymap

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -6,3 +6,9 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+-- Window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to below window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to above window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
+
