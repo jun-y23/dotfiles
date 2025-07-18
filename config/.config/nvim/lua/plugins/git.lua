ed073a6c5b05628b8e64e4c3078dd307bf41a96b
jun-y23 <ki8109smith@gmail.com>
2025-07-19 00:14:37 +0900

    chore: diffgit

--- config/.config/nvim/lua/plugins/git.lua
+++ config/.config/nvim/lua/plugins/git.lua
@@ -1,20 +1,29 @@
 return {
-  "kdheepak/lazygit.nvim",
-  lazy = true,
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    },
   },
-  -- optional for floating window border decoration
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  -- setting the keybinding for LazyGit with 'keys' is recommended in
-  -- order to load the plugin when the command is run for the first time
-  keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>hd", "<cmd>DiffviewOpen HEAD~1<cr>",   desc = "View Diff" },
+      { "<leader>hf", "<cmd>DiffviewFileHistory %<cr>", desc = "View File History" },
+    }
   }
 }
